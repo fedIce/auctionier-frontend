a429dfafabef91680b10b9084aafe6cfcc3f2ba5
fedIce <freedez1996@gmail.com>
2025-07-08 15:41:55 +0300

    7/7/25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ListingCardsSection from "../components/ListingCardsSection";
 
 export default function Home() {
   return (
-    <div className="w-full h-auto flex flex-col justify-start items-center">
+    <div className="w-full h-auto flex flex-col text-bright justify-start items-center">
       <main className="w-full h-full max-w-7xl flex flex-col items-center pt-8">
 
         <div className="w-full flex justify-center max-w-4xl mt-8 lg:mt-20">
